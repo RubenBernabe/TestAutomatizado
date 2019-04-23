9c61cf610c0b562fedb9c9eec6e489f29d226927
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2019-04-24 00:00:42 +0200

    ju

--- renfe_test/features/support/env.rb
+++ renfe_test/features/support/env.rb
@@ -3,7 +3,7 @@ require 'selenium-webdriver'
 require 'capybara/rspec'
 
 Capybara.default_driver = :selenium_chrome
-Capybara.ignore_hidden_elements = true
+Capybara.ignore_hidden_elements = false
 Capybara.default_selector = :css
 
 
