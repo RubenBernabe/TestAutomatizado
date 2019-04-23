ba2f3f569f29de257655dc4f783da47cfdd56594
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2019-04-24 00:04:39 +0200

    Update capybara.rb

--- youtube_test/features/support/capybara.rb
+++ youtube_test/features/support/capybara.rb
@@ -1,5 +1,9 @@
 require 'capybara/cucumber'
+
+require 'selenium-webdriver'
 Capybara.default_driver = :selenium_chrome
+
 Capybara.ignore_hidden_elements = false
+
 Capybara.default_selector = :css
 
